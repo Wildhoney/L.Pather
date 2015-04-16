b77f7a907eba634c81a1011e08089f9c830e1dbc
Wildhoney <adam.timberlake@gmail.com>
2015-04-17 00:50:45 +0100

    Added initial path drawing

--- module/Pather.js
+++ module/Pather.js
@@ -31,7 +31,8 @@
          * @return {void}
          */
         initialize: function initialize(options) {
-            this.options = options || {};
+            this.options  = options || this.defaultOptions();
+            this.creating = false;
         },
 
         /**
@@ -68,7 +69,64 @@
          * @return {void}
          */
         onAdd: function onAdd(map) {
-            return void map;
+
+            map.dragging.disable();
+
+            var element    = this.options.element || map._container;
+            this.fromPoint = { x: 0, y: 0 };
+            this.svg       = d3.select(element)
+                               .append('svg')
+                                   .attr('pointer-events', 'none')
+                                   .attr('class', this.getOption('moduleClass'))
+                                   .attr('width', this.getOption('width'))
+                                   .attr('height', this.getOption('height'));
+
+            // Attach the mouse events for drawing the polyline.
+            this.attachEvents(map);
+
+        },
+
+        /**
+         * @method attachEvents
+         * @param {L.Map} map
+         * @return {void}
+         */
+        attachEvents: function attachEvents(map) {
+
+            map.on('mousedown', function mousedown(event) {
+                this.creating  = true;
+                this.fromPoint = map.latLngToContainerPoint(event.latlng);
+            }.bind(this));
+
+            map.on('mouseup', function mouseup() {
+                this.creating = false;
+            }.bind(this));
+
+            map.on('mousemove', function mousemove(event) {
+
+                var lineFunction = d3.svg.line()
+                                     .x(function x(d) { return d.x; })
+                                     .y(function y(d) { return d.y; })
+                                     .interpolate('linear');
+
+                if (this.creating) {
+
+                    var point    = map.mouseEventToContainerPoint(event.originalEvent),
+                        lineData = [this.fromPoint, new L.Point(point.x, point.y, false)];
+
+                    this.svg.append('path')
+                            .classed(this.getOption('lineClass'), true)
+                                .attr('d', lineFunction(lineData))
+                                .attr('stroke', this.getOption('strokeColour'))
+                                .attr('stroke-width', this.getOption('strokeWidth'))
+                                .attr('fill', 'none');
+
+                    this.fromPoint = { x: point.x, y: point.y };
+
+                }
+
+            }.bind(this));
+
         },
 
         /**
@@ -77,6 +135,32 @@
          */
         clearAll: function clearAll() {
             d3.select('svg').text('');
+        },
+
+        /**
+         * @method getOption
+         * @param {String} property
+         * @return {String|Number}
+         */
+        getOption: function getOption(property) {
+            return this.options[property] || this.defaultOptions()[property];
+        },
+
+        /**
+         * @method defaultOptions
+         * @return {Object}
+         */
+        defaultOptions: function defaultOptions() {
+
+            return {
+                moduleClass: 'pather',
+                strokeColour: '#D7217E',
+                strokeWidth: 2,
+                lineClass: 'drawing-line',
+                width: '100%',
+                height: '100%'
+            };
+
         }
 
     });
